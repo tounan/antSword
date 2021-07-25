a2e22e7189bef70f4a7e02c678606e9700027c84
Medicean <Medici.Yan@Gmail.com>
2021-07-25 11:49:02 +0800

    (Enhance: Core/CMDLINUX) 内置hex解码器增加python3

--- source/core/cmdlinux/decoder/hex.js
+++ source/core/cmdlinux/decoder/hex.js
@@ -9,7 +9,14 @@
     * @returns {string} asenc 加密返回数据的函数
     */
    asoutput: () => {
-     return `asenc(){ xxd -ps "$@"; };`
+     return `asenc(){ 
+      command_exists() { command -v "$@" > /dev/null 2>&1; };
+      if command_exists xxd; then 
+        xxd -ps "$@";
+      elif command_exists python3; then 
+        echo -n "$@"|python3 -c "import sys, binascii; sys.stdout.buffer.write(binascii.hexlify(input().strip().encode()))"; 
+      fi
+     };`.replace(/\n\s+/g, '')
    },
    /**
     * 解码 Buffer
